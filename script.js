67af43c2f27c3b878b9e90f197f81045a3ca6b4e
Atticus King <attking@ucdavis.edu>
2024-04-05 18:03:34 -0700

    fixed files

--- script.js
+++ script.js
@@ -3,27 +3,74 @@
 
     const button = document.querySelector('button');
     const body = document.querySelector('body');
-    const banner = document.querySelector('#banner');
-    const sections = document.querySelectorAll('section')
+    const h1 = document.getElementById('h1');
+    const homeis = document.getElementById('homeis');
+    const title = document.getElementById('title');
+
+    const brownMainSections = document.querySelectorAll('.brownmain');
+
+    const footer = document.querySelector('footer');
+
+    
     let mode = 'dark';
 
     button.addEventListener('click', function() {
+        
         if (mode === 'dark') {
-            body.className = 'switch';
-            banner.className = 'switch';
-            button.className = 'switch';
-            for (const section of sections) {
-                section.className = 'switch';
-            }
+            button.classList.remove('buttonbrown');
+            button.classList.add('buttonblue');
+            body.classList.remove('pastbody');
+            body.classList.add('futurebody');
+
+            h1.classList.remove('h1brown');
+            h1.classList.add('h1white');
+            homeis.classList.remove('homeis1');
+            homeis.classList.add('homeis2');
+            title.classList.remove('pasttitle');
+            title.classList.add('futuretitle');
+            title.textContent = "The Future";
+
+
+            brownMainSections.forEach(section => {
+                section.classList.toggle('brownmain');
+                section.classList.toggle('bluemain');
+            });
+
+
+            footer.classList.remove('footerbrown');
+            footer.classList.add('footerblue');
+            
+            
+
             mode = 'light';
-        } else {
-            body.removeAttribute('class');
-            banner.removeAttribute('class');
-            button.removeAttribute('class');
-            for (const section of sections) {
-                section.removeAttribute('class');
-            }
-            mode = 'dark'
+
+
+        } else if (mode === 'light') {
+            button.classList.remove('buttonblue');
+            button.classList.add('buttonbrown');
+            body.classList.remove('futurebody');
+            body.classList.add('pastbody');
+            
+            h1.classList.remove('h1white');
+            h1.classList.add('h1brown');
+            homeis.classList.remove('homeis2');
+            homeis.classList.add('homeis1');
+            title.classList.remove('futuretitle');
+            title.classList.add('pasttitle');
+            title.textContent = "The Past";
+
+
+            brownMainSections.forEach(section => {
+                section.classList.toggle('brownmain');
+                section.classList.toggle('bluemain');
+            });
+
+
+            footer.classList.remove('footerblue');
+            footer.classList.add('footerbrown');
+
+
+            mode = 'dark';
         }
-    })
-})()
+    });
+})();
